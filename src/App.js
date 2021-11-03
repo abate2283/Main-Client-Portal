cb9d222230843b57df47ce29886d35c813a623b5
Alfred A Bate <alfredbate@yahoo.co.uk>
2021-11-03 09:59:48 -0500

    mistake in first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>The Introduction</h1>
+      <h3>
+        <p> What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+  when an unknown printer took a galley of type and scrambled it to make a type
+   specimen book. It has survived not only five centuries, but also the leap 
+   into electronic typesetting, remaining essentially unchanged. It was popularised 
+   in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+   and more recently with desktop publishing software like Aldus PageMaker including 
+   versions of Lorem Ipsum.</p></h3>
+   <table>
+     <thead>
+       <tr>
+         <td>Blog</td>
+         <td>Post</td>
+         <td>Comments</td>
+         <td>Replies</td>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <td>Lets talk about Jesus</td>
+         <td>And what he has done?</td>
+         <td>How He sets you free</td>
+         <td>From sin and death.  You know what that means?</td>
+       </tr>
+     </tbody>   
+   </table>
+
+        
+          
+    
     </div>
   );
 }
